debf814e25967257fe741bd1ecff6f261f5ccd45
rajnikanthole <>
2024-03-18 23:53:57 +0530

    added

---
+++ SwiftUIExample/SwiftUIExample/SwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExampleApp.swift
+//  SwiftUIExample
+//
+//  Created by rajnikanthole on 18/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
